f22b3544fb91414dcfec9c1e2a8495b325b636bd
Markus Blaschke <mblaschke82@gmail.com>
2022-03-22 21:17:47 +0100

    cleanup
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- config/opts.go
+++ config/opts.go
@@ -24,13 +24,13 @@ type (
 		// scrape times
 		Cronjobs struct {
 			KeyvaultAccessPolicies string `long:"cron.keytvaultaccesspolicies" env:"CRON_KEYTVAULTACCESSPOLICIES"  description:"Cronjob for KeyVault AccessPolicies report" default:"0 * * * *"`
-			ResourceGroups         string `long:"cron.resourcegroups" env:"CRON_RESOURCEGROUPS"  description:"Cronjob for ResourceGroups report" default:"*/30 * * * *"`
-			ResourceProvider       string `long:"cron.resourceproviders" env:"CRON_RESOURCEPROVIDERS"  description:"Cronjob for ResourceProviders report" default:"0 * * * *"`
-			RoleAssignments        string `long:"cron.roleassignments" env:"CRON_ROLEASSIGNMENTS"  description:"Cronjob for RoleAssignments report" default:"*/5 * * * *"`
+			ResourceGroups         string `long:"cron.resourcegroups"          env:"CRON_RESOURCEGROUPS"           description:"Cronjob for ResourceGroups report"          default:"*/30 * * * *"`
+			ResourceProvider       string `long:"cron.resourceproviders"       env:"CRON_RESOURCEPROVIDERS"        description:"Cronjob for ResourceProviders report"       default:"0 * * * *"`
+			RoleAssignments        string `long:"cron.roleassignments"         env:"CRON_ROLEASSIGNMENTS"          description:"Cronjob for RoleAssignments report"         default:"*/5 * * * *"`
 		}
 
 		Config string `long:"config"  env:"CONFIG"   description:"Config file path"     required:"true"`
-		DryRun bool   `long:"dry-run"  env:"DRYRUN"    description:"Dry Run (report only)"`
+		DryRun bool   `long:"dry-run"  env:"DRYRUN"  description:"Dry Run (report only)"`
 
 		// general options
 		ServerBind string `long:"bind" env:"SERVER_BIND"   description:"Server address"     default:":8080"`
